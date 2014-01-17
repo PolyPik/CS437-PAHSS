4a001e2ebf84a13a83c88f9116a7fd70fb67d7ef
Abraham <abeponce@gmail.com>
2014-01-16 19:50:25 -0800

    Database code

---
+++ pahss/src/test/Database.java
@@ -0,0 +1,35 @@
+package test;
+import java.sql.*;
+
+public class Database {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url = "jdbc:mysql://localhost/Aquarium";
+		String username = "root";
+		String password = "";
+		
+		try
+		{
+			Connection connection = null;
+			System.out.println("Connecting to database...\n");
+			connection = DriverManager.getConnection(url, username,
+					password);
+			Statement stmt = connection.createStatement();
+			String query = "select * from Aquarium";
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next())
+			{
+				System.out.println(rs.getString("name") + " " + rs.getString("id"));
+			}
+			connection.close();
+			System.out.println("\nConnection closed.");
+		}
+		catch(SQLException s)
+		{
+			System.out.println("\nNo Connection.");
+			System.out.println(s);
+		}
+	}
+
+}
