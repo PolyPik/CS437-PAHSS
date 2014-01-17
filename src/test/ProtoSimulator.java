4f59e08011b25cc3d18e9d2fd4a4f7b27ea8c197
csula <csula@localhost.localdomain>
2014-01-16 18:40:44 -0800

    First Commit
    Only some prototypes
    Signed-off-by: Howard Li <csula@localhost.localdomain>

---
+++ src/test/ProtoSimulator.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ProtoSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hostName = "localhost";
+		int portNumber = 1603;
+	}
+    
+    
+
+}
