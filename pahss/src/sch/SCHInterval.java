94377232885361bd83301f3da38291d9ec1ecd2d
Polypik <csula@localhost.localdomain>
2014-02-13 22:12:54 -0800

    Yet another minor push

--- pahss/src/sch/SCHInterval.java
+++ pahss/src/sch/SCHInterval.java
@@ -1,5 +1,22 @@
 package sch;
 
 public class SCHInterval {
-
+	private int start_hour;
+	private int start_minute;
+	private int stop_hour;
+	private int stop_minute;
+	
+	public SCHInterval() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setStarttime(int hour, int minute){
+		start_hour = hour;
+		start_minute = minute;
+	}
+	
+	public void setStoptime(int hour, int minute){
+		stop_hour = hour;
+		stop_minute = minute;
+	}
 }
