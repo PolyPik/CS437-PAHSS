336b29d8346f69368ae6f501c10368e5ba7a5859
Polypik <csula@localhost.localdomain>
2014-02-18 16:20:48 -0800

    02/18/14 Commit

--- pahss/src/sch/SCHInterval.java
+++ pahss/src/sch/SCHInterval.java
@@ -19,4 +19,27 @@ public class SCHInterval {
 		stop_hour = hour;
 		stop_minute = minute;
 	}
+	
+	public boolean checkOverlap(SCHInterval o){
+		float start_val1 = (float) (this.start_hour+(this.start_minute/60.0));
+		float stop_val1 = (float) (this.stop_hour+(this.stop_minute/60.0));
+		float start_val2 = (float) (o.start_hour+(o.start_minute/60.0));
+		float stop_val2 = (float) (o.stop_hour+(o.stop_minute/60.0));
+		
+		if(stop_val1 < start_val1){
+			if(stop_val2 < start_val2){
+				return true;
+			}else if((start_val2<=stop_val1)||(stop_val2>=start_val1)){
+				return true;
+			} else{
+				return false;
+			}
+		} else{
+			if((start_val2<=stop_val1)||(stop_val2>=start_val1)){
+				return true;
+			} else{
+				return false;
+			}
+		}
+	}
 }
