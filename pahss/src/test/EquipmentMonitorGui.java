ee8a5ad5fbddd5b8d68f4b28a55bd27024795f66
lacsula <luischavez001@gmail.com>
2014-02-25 20:57:38 -0800

    New Classes
    Created a new GuiTester and a copy of the EquipmentMonitor. I plan to
    have a rough draft of the Gui built soon, so I can integrate it with the
    copy of EquipmentMonitor.

---
+++ pahss/src/test/EquipmentMonitorGui.java
@@ -0,0 +1,248 @@
+package test;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class EquipmentMonitorGui {
+	ArrayList<EquipmentCheckGui> EquipmentCheckGuiList = new ArrayList<EquipmentCheckGui>();
+
+	public ArrayList<EquipmentCheckGui> getCheckList() {
+		return EquipmentCheckGuiList;
+	}
+
+	// replaces monitor.getCheckList.get(i);
+	public EquipmentCheckGui getCheckListValue(int i) {
+		return EquipmentCheckGuiList.get(i);
+	}
+
+	// replaces monitor.getCheckList.add(i);
+	public void addToCheckList(EquipmentCheckGui e) {
+		EquipmentCheckGuiList.add(e);
+	}
+}
+
+class EquipmentCheckGui extends Thread {
+	String equipmentName = "Unknown Equipment";
+	String errorNotice = "Equipment is broken! Fix immediately!";
+	int tank_id;
+	int equipment_id;
+	boolean isOperational = true;
+	boolean isChecked = false;
+
+	EquipmentCheckGui(int tank_id, int equipment_id) {
+		this.tank_id = tank_id;
+		this.equipment_id = equipment_id;
+		/*
+		 * instead of all these if statements, equipmentName will take the
+		 * equipment id, look for it on the table equipment_checks in the
+		 * database, and assigns itself to whatever name it finds there. This is
+		 * an example of the query it will use:
+		 * 
+		 * select equipment_name from equipment_checks where equipment_id = 1;
+		 */
+		if (equipment_id == 1)
+			equipmentName = "Heater";
+		else if (equipment_id == 2)
+			equipmentName = "Chiller";
+		else if (equipment_id == 3)
+			equipmentName = "Chemical dispenser";
+		else if (equipment_id == 4)
+			equipmentName = "Feeder";
+		else if (equipment_id == 5)
+			equipmentName = "Lighting";
+		else if (equipment_id == 6)
+			equipmentName = "Air pump";
+		else if (equipment_id == 7)
+			equipmentName = "Filtration";
+		else if (equipment_id == 8)
+			equipmentName = "Water pump";
+		else if (equipment_id == 9)
+			equipmentName = "Temperature sensor";
+		else if (equipment_id == 10)
+			equipmentName = "Salinity sensor";
+		else if (equipment_id == 11)
+			equipmentName = "ph sensor";
+		else if (equipment_id == 12)
+			equipmentName = "Ammonia sensor";
+		else if (equipment_id == 13)
+			equipmentName = "Nitrite sensor";
+		else if (equipment_id == 14)
+			equipmentName = "Nitrate sensor";
+		else if (equipment_id == 15)
+			equipmentName = "Oxygen sensor";
+		else if (equipment_id == 16)
+			equipmentName = "Water hardness sensor";
+		else if (equipment_id == 17)
+			equipmentName = "Reservoir water level sensor";
+		else if (equipment_id == 18)
+			equipmentName = "Feeder food supply";
+		else if (equipment_id == 19)
+			equipmentName = "Chemical supply dispenser";
+		/*
+		 * The same process that equipmentName went through will also occur for
+		 * errorNotice, except it will use the value found in the error_message
+		 * field. This is an example of the query it will use:
+		 * 
+		 * select error_message from equipment_checks where equipment_id = 1;
+		 */
+		errorNotice = equipmentName + " is broken! Please fix immediately!";
+	}
+
+	public void setOperational(boolean operational) {
+		this.isOperational = operational;
+	}
+
+	public boolean getOperational() {
+		return isOperational;
+	}
+
+	public void setChecked(boolean checked) {
+		this.isChecked = checked;
+	}
+
+	public boolean getChecked() {
+		return isChecked;
+	}
+
+	public void run() {
+		while (true) {
+			/*
+			 * The following code simulates how the Equipment Monitor would keep
+			 * track of the boolean value. If we had a table in the database
+			 * with booleans, this is what would be added: When you declare an
+			 * object in the main method, you would specify which equipment from
+			 * which tank is being read. This is done by passing in the specific
+			 * tank_id and equipment_id. Then, this method will use those ID's
+			 * to figure out which boolean value in the database to constantly
+			 * read. Whenever that value is set to false, an error message will
+			 * be sent to MI. Otherwise, print out that everything is fine. In
+			 * order to keep the internal value of the boolean update, we might
+			 * have to continuously keep reading from the database. That means
+			 * we'll either have to create a variable right before the checks
+			 * below that will keep reading based on id's, or just use the code
+			 * that would access the database inside the booleans of the checks
+			 * below. More work on this is needed.
+			 * 
+			 * Basically, instead of using getOperational(), which is a boolean
+			 * that is toggled by user input, we will instead use the tank_id
+			 * and equipment_id of this object to find the corresponding boolean
+			 * value in the operational_status column of the equipment_status
+			 * table in the database. Alternatively, while the program is
+			 * running, we could set getOperational = boolean found in
+			 * operational_status of the equipment_status table. This would be
+			 * the query used to retrieve the value:
+			 * 
+			 * select operational_status from equipment_status where tank_id = 1
+			 * and equipment_id = 1;
+			 */
+
+			if (!getOperational() && !isChecked) {
+				System.out.println("ERROR: Tank " + tank_id + "'s "
+						+ equipmentName + " MALFUNCTION: " + errorNotice);
+				setChecked(true);
+			}
+			if (getOperational() && isChecked) {
+				setChecked(false);
+				System.out.println("Error has been fixed on Tank " + tank_id
+						+ "'s " + equipmentName);
+			}
+
+			// *
+			try {
+				sleep(2000);
+			} catch (InterruptedException e) {
+				System.out.println("sleep did not work");
+			}
+			// */
+		}
+	}
+
+	public static void main(String[] args) {
+
+		/*
+		 * The EquipmentMonitorGui class has an ArrayList EquipmentCheckGuiList. The
+		 * main method of EquipmenentCheck will create an object of type
+		 * EquipmentMonitorGui, and proceed to add elements to it of type
+		 * EquipmentCheckGui. When we implement the database, these objects will
+		 * also take in the tank_id and the equipment_id of the equipment they
+		 * represent. These values will correspond to a boolean value that will
+		 * have a value of either true or false. This value will be changed by
+		 * the other modules whenever equipment malfunctions, by referencing the
+		 * table based on the specific tank_id and equipment_id of the equipment
+		 * and tank that malfunctioned. During the object's run method, whenever
+		 * a value is false, an error message will be printed and sent to the
+		 * MI.
+		 */
+
+		/*
+		 * Here, we are allowing the user to determine the number of tanks.
+		 * Instead, we can automatically detect how many tanks there are using
+		 * this query:
+		 * 
+		 * select count(id) from tank_database;
+		 */
+		EquipmentMonitorGui monitor = new EquipmentMonitorGui();
+		Scanner input = new Scanner(System.in);
+		System.out
+				.println("Enter the number of default tanks (preferably 1-3):");
+		int numberOfTanks = input.nextInt();
+
+		for (int t = 1; t <= numberOfTanks; t++) {
+			for (int e = 1; e <= 19; e++)
+				monitor.addToCheckList(new EquipmentCheckGui(t, e));
+		}
+
+		for (int i = 0; i < monitor.getCheckList().size(); i++)
+			monitor.getCheckListValue(i).start();
+
+		/*
+		 * This is a way to test the true/false case. Need to be able to
+		 * consider information of all equipment for every tank. Perhaps the
+		 * modules in charge of the equipment can specify which tank is causing
+		 * the error when errors occur. They will then send the tank id and
+		 * equipment id, and based on those id's, the corresponding tank will
+		 * have its boolean set to false. Here are examples of queries that
+		 * would be used to modify the boolean values in the database:
+		 * 
+		 * update equipment_status set operational_status = 0 where tank_id = 1
+		 * and equipment_id = 1;
+		 * 
+		 * update equipment_status set operational_status = 1 where tank_id = 1
+		 * and equipment_id = 1;
+		 */
+
+		System.out.println("All equipment currently working.");
+		System.out.println("Input numbers 0 through "
+				+ (numberOfTanks * 19 - 1)
+				+ " to break/fix equipment and display error/fix message:");
+		while (true) {
+			int boolSwitch = input.nextInt();
+			if (boolSwitch >= 0 && boolSwitch < monitor.getCheckList().size())
+				monitor.getCheckListValue(boolSwitch)
+						.setOperational(
+								!monitor.getCheckListValue(boolSwitch)
+										.getOperational());
+		}
+
+		/*
+		 * The logic for this is nearly complete. This program needs to handle
+		 * the case for when new tanks/equipment are added. For each new tank,
+		 * 19 new equipment checks are added to the database, which would mean
+		 * 19 new values must be added to the array list, and they must all be
+		 * filled. Not sure how to detect that, but here's what I think:
+		 * 
+		 * Have this program constantly check the state of the equipment_status
+		 * table. If, at any time, new tanks are added, then for each new tank,
+		 * 19 entries are added. Then this program will use the new entries'
+		 * tank_id and equipment_id to create a new EquipmentCheckGui object and
+		 * add it to the array list.
+		 * 
+		 * ALTERNATIVELY, if this program is going to be turned off whenever
+		 * tanks are added, then it would be better to recalculate the number of
+		 * tanks. In that case, this program needs to be changed such that
+		 * instead of asking the user how many tanks there are, it will instead
+		 * check the tank_database and count how many tanks there are, and then
+		 * just build the array list all over again.
+		 */
+	}
+}
--- pahss/src/test/GuiTester2.java
+++ pahss/src/test/GuiTester2.java
@@ -1,15 +1,22 @@
 package test;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import javax.swing.*;
 
 /*
  * This won't be part of the final project. It's just a class I'm using to experiment with JFrame for the GUI for this project.
+ * Additionally, this, when finalized (it's not yet), may become integrated with EquipmentMonitor
  * */
 
-public class GuiTester {
+public class GuiTester2 {
 
 	public static void createWindow() {
+		ArrayList<JLabel> textLabelList = new ArrayList<JLabel>();
+		ArrayList<JTextArea> textAreaList = new ArrayList<JTextArea>();
+		
 		JFrame myFrame = new JFrame("Equipment Monitor");
 		
 		JLabel label;
@@ -27,68 +34,69 @@ public class GuiTester {
 		label.setFont(new Font("Serif", Font.BOLD, 30));
 		myFrame.add(label, c);
 		
-		label = new JLabel("Tank 1");
+		JLabel labelT1 = new JLabel("Tank 1");
 		c.weightx = 0.5;
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 1;
-		label.setFont(new Font("Serif", Font.BOLD, 20));
-		myFrame.add(label, c);
+		labelT1.setFont(new Font("Serif", Font.BOLD, 20));
+		myFrame.add(labelT1, c);
 
-		label = new JLabel("Tank 2");
+		JLabel labelT2 = new JLabel("Tank 2");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridx = 1;
 		c.gridy = 1;
-		label.setFont(new Font("Serif", Font.BOLD, 20));
-		myFrame.add(label, c);
+		labelT2.setFont(new Font("Serif", Font.BOLD, 20));
+		myFrame.add(labelT2, c);
 
-		label = new JLabel("Tank 3");
+		JLabel labelT3 = new JLabel("Tank 3");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridx = 2;
 		c.gridy = 1;
-		label.setFont(new Font("Serif", Font.BOLD, 20));
-		myFrame.add(label, c);
+		labelT3.setFont(new Font("Serif", Font.BOLD, 20));
+		myFrame.add(labelT3, c);
 
-		JTextArea textArea = new JTextArea("Tank 1 data", 10, 10);
-		JScrollPane scrollPane = new JScrollPane(textArea);
+		JTextArea textArea1 = new JTextArea("Tank 1 data\n", 10, 10);
+		JScrollPane scrollPane1 = new JScrollPane(textArea1);
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.insets = new Insets(0,5,0,5);
 		c.weightx = 0.5;
 		c.gridwidth = 1;
 		c.gridx = 0;
 		c.gridy = 2;
-		textArea.setEditable(true);
-		textArea.setRows(20);
-		textArea.setColumns(20);
-		myFrame.getContentPane().add(scrollPane,c);
+		textArea1.setEditable(true);
+		textArea1.setRows(20);
+		textArea1.setColumns(20);
+		myFrame.getContentPane().add(scrollPane1,c);
+		//textArea1.append("new text"); //this will be the basis for updating the text
 		
-		textArea = new JTextArea("Tank 2 data", 10, 10);
-		scrollPane = new JScrollPane(textArea);
+		JTextArea textArea2 = new JTextArea("Tank 2 data\n", 10, 10);
+		JScrollPane scrollPane2 = new JScrollPane(textArea2);
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridwidth = 1;
 		c.gridx = 1;
 		c.gridy = 2;
-		textArea.setEditable(true);
-		textArea.setRows(20);
-		textArea.setColumns(20);
-		myFrame.getContentPane().add(scrollPane,c);
+		textArea2.setEditable(true);
+		textArea2.setRows(20);
+		textArea2.setColumns(20);
+		myFrame.getContentPane().add(scrollPane2,c);
 		
-		textArea = new JTextArea("Tank 3 data", 10, 10);
-		scrollPane = new JScrollPane(textArea);
+		JTextArea textArea3 = new JTextArea("Tank 3 data\n", 10, 10);
+		JScrollPane scrollPane3 = new JScrollPane(textArea3);
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridwidth = 1;
 		c.gridx = 2;
 		c.gridy = 2;
-		textArea.setEditable(true);
-		textArea.setRows(20);
-		textArea.setColumns(20);
-		myFrame.getContentPane().add(scrollPane,c);
+		textArea3.setEditable(true);
+		textArea3.setRows(20);
+		textArea3.setColumns(20);
+		myFrame.getContentPane().add(scrollPane3,c);
 
-		label = new JLabel("End of window");
+		JLabel labelEnd = new JLabel("End of window");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.ipady = 0;       //reset to default
 		c.weighty = 1.0;   //request any extra vertical space
@@ -97,7 +105,7 @@ public class GuiTester {
 		c.gridx = 1;       //aligned with button 2
 		c.gridwidth = 2;   //2 columns wide
 		c.gridy = 3;       //third row
-		myFrame.add(label, c);
+		myFrame.add(labelEnd, c);
 		
 		myFrame.setSize(800, 500);
 		myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -108,6 +116,12 @@ public class GuiTester {
 	}
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input how many tanks are in aquarium: ");
+		int numTanks = input.nextInt();
+		//this will determine how many headings and how many text boxes there are
+		//remember to implement an array list
+		
 		createWindow();
 	}
 
